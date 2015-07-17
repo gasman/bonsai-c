4b2e05a6eb01a0b9a89a393152d44979b8fdd92e
Matt Westcott <matt@west.co.tt>
2015-07-17 20:08:35 +0100

    Add test runner

--- bonsai-c.js
+++ bonsai-c.js
@@ -6,11 +6,23 @@ parser.yy = require("./ast");
 
 var compiler = require("./compiler");
 
-var cSource = fs.readFileSync(process.argv[2], "utf8");
-var ast = parser.parse(cSource);
-console.log(util.inspect(ast, { depth: null }));
+exports.compile = function(filename) {
+	var cSource = fs.readFileSync(filename, "utf8");
+	var ast = parser.parse(cSource);
+	return compiler.compileModule('Module', ast);
+}
 
-console.log("\n---------\n");
+exports.main = function(argv) {
+	var cSource = fs.readFileSync(argv[2], "utf8");
+	var ast = parser.parse(cSource);
+	console.log(util.inspect(ast, { depth: null }));
 
-var output = compiler.compileModule('Module', ast);
-console.log(output);
+	console.log("\n---------\n");
+
+	var output = compiler.compileModule('Module', ast);
+	console.log(output);
+}
+
+if (typeof module !== 'undefined' && require.main === module) {
+	exports.main(process.argv);
+}
---
+++ runtests.js
@@ -0,0 +1,10 @@
+var BonsaiC = require('./bonsai-c');
+var assert = require('assert');
+
+var js, module;
+
+js = BonsaiC.compile('tests/fortytwo.c');
+module = eval('(' + js + ')')();
+assert.equal(42, module.main());
+
+console.log("All tests passed");
