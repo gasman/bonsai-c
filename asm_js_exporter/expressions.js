877f9d9728450d8f1b4957446938024a205c11da
Matt Westcott <matt@west.co.tt>
2017-06-22 02:17:46 +0100

    Refactor expressions module to expose expression object constructors

--- asm_js_exporter/expressions.js
+++ asm_js_exporter/expressions.js
@@ -4,6 +4,100 @@ var util = require('util');
 var estree = require('./estree');
 var types = require('./types');
 
+function AddExpression(left, right) {
+	var typ;
+	if (left.type.satisfies(types.int) && right.type.satisfies(types.int)) {
+		typ = types.intish;
+	} else {
+		throw(
+			util.format("Can't handle AddExpression with operand types %s and %s",
+				util.inspect(left.type),
+				util.inspect(right.type)
+			)
+		);
+	}
+	return {
+		'tree': estree.BinaryExpression('+',
+			wrapFunctionCall(left).tree,
+			wrapFunctionCall(right).tree
+		),
+		'type': typ
+	};
+}
+exports.AddExpression = AddExpression;
+
+function AssignmentExpression(left, right) {
+	assert(left.isValidAsLvalue,
+		"Left hand side of an assignment must be a valid lvalue - got " + util.inspect(left)
+	);
+
+	right = coerce(right, left.intendedType);
+
+	return {
+		'tree': estree.AssignmentExpression('=',
+			left.tree,
+			wrapFunctionCall(right).tree
+		),
+		'type': right.type
+	};
+}
+exports.AssignmentExpression = AssignmentExpression;
+
+function ConstExpression(value) {
+	var typ = null;
+	/* FIXME: Should infer type from the numeric form in the original source code;
+	e.g. 10.0 should be treated as a float/double, not an integer */
+	if (Number.isInteger(value)) {
+		if (value >= 0 && value < 0x80000000) {
+			typ = types.fixnum;
+		} else if (value >= 0x80000000 && value < 0x100000000) {
+			typ = types.unsigned;
+		}
+	}
+	if (typ === null) {
+		throw("Can't determine type of numeric literal: " + value);
+	}
+	return {
+		'tree': estree.Literal(value),
+		'type': typ,
+		'isDirectNumericLiteral': true,
+		'isNumericLiteral': true,
+		'numericLiteralValue': value
+	};
+}
+exports.ConstExpression = ConstExpression;
+
+function FunctionCallExpression(callee, args) {
+	assert(callee.isIdentifier,
+		"Callee of a function call must be an identifier - got " + util.inspect(callee)
+	);
+
+	var expectedParamTypes = callee.type.paramTypes;
+
+	var argTrees = [];
+	for (var i = 0; i < args.length; i++) {
+		var arg = wrapFunctionCall(coerce(args[i], expectedParamTypes[i]));
+		argTrees.push(arg.tree);
+	}
+	return {
+		'tree': estree.CallExpression(callee.tree, argTrees),
+		'type': callee.type.returnType,
+		'isFunctionCall': true
+	};
+}
+exports.FunctionCallExpression = FunctionCallExpression;
+
+function VariableExpression(variable) {
+	return {
+		'tree': estree.Identifier(variable.name),
+		'type': variable.type,
+		'intendedType': variable.intendedType,
+		'isIdentifier': true,
+		'isValidAsLvalue': true
+	};
+}
+exports.VariableExpression = VariableExpression;
+
 function compileExpression(expression, context) {
 	var left, right, arg, typ;
 
@@ -11,84 +105,20 @@ function compileExpression(expression, context) {
 		case 'AddExpression':
 			left = compileExpression(expression.left, context);
 			right = compileExpression(expression.right, context);
-			if (left.type.satisfies(types.int) && right.type.satisfies(types.int)) {
-				typ = types.intish;
-			} else {
-				throw(
-					util.format("Can't handle AddExpression with operand types %s and %s",
-						util.inspect(left.type),
-						util.inspect(right.type)
-					)
-				);
-			}
-			return {
-				'tree': estree.BinaryExpression('+',
-					wrapFunctionCall(left).tree,
-					wrapFunctionCall(right).tree
-				),
-				'type': typ
-			};
+			return AddExpression(left, right);
 		case 'AssignmentExpression':
 			left = compileExpression(expression.left, context);
-			assert(left.isValidAsLvalue,
-				"Left hand side of an assignment must be a valid lvalue - got " + util.inspect(left)
-			);
-
-			right = coerce(
-				compileExpression(expression.right, context),
-				left.intendedType
-			);
-
-			return {
-				'tree': estree.AssignmentExpression('=',
-					left.tree,
-					wrapFunctionCall(right).tree
-				),
-				'type': right.type
-			};
+			right = compileExpression(expression.right, context);
+			return AssignmentExpression(left, right);
 		case 'ConstExpression':
-			typ = null;
-			/* FIXME: Should infer type from the numeric form in the original source code;
-			e.g. 10.0 should be treated as a float/double, not an integer */
-			if (Number.isInteger(expression.value)) {
-				if (expression.value >= 0 && expression.value < 0x80000000) {
-					typ = types.fixnum;
-				} else if (expression.value >= 0x80000000 && expression.value < 0x100000000) {
-					typ = types.unsigned;
-				}
-			}
-			if (typ === null) {
-				throw("Can't determine type of numeric literal: " + expression.value);
-			}
-			return {
-				'tree': estree.Literal(expression.value),
-				'type': typ,
-				'isDirectNumericLiteral': true,
-				'isNumericLiteral': true,
-				'numericLiteralValue': expression.value
-			};
+			return ConstExpression(expression.value);
 		case 'FunctionCallExpression':
 			var callee = compileExpression(expression.callee, context);
-
-			assert(callee.isIdentifier,
-				"Callee of a function call must be an identifier - got " + util.inspect(callee)
-			);
-
-			var expectedParamTypes = callee.type.paramTypes;
-
-			var argTrees = [];
+			var args = [];
 			for (var i = 0; i < expression.parameters.length; i++) {
-				arg = wrapFunctionCall(coerce(
-					compileExpression(expression.parameters[i], context),
-					expectedParamTypes[i]
-				));
-				argTrees.push(arg.tree);
+				args[i] = compileExpression(expression.parameters[i], context);
 			}
-			return {
-				'tree': estree.CallExpression(callee.tree, argTrees),
-				'type': callee.type.returnType,
-				'isFunctionCall': true
-			};
+			return FunctionCallExpression(callee, args);
 		case 'NegationExpression':
 			arg = compileExpression(expression.argument, context);
 			typ = null;
@@ -123,13 +153,7 @@ function compileExpression(expression, context) {
 					throw "Variable not found: " + util.inspect(expression.variable);
 				}
 			}
-			return {
-				'tree': estree.Identifier(variable.name),
-				'type': variable.type,
-				'intendedType': variable.intendedType,
-				'isIdentifier': true,
-				'isValidAsLvalue': true
-			};
+			return VariableExpression(variable);
 		default:
 			throw "Unexpected expression type: " + expression.expressionType;
 	}
