0288bcdaaf383b7deb821ab2bc52e0da254f8eaa
Matt Westcott <matt@west.co.tt>
2017-07-30 13:46:25 +0100

    implement infrastructure for compiling function definitions to wast

---
+++ wasm_exporter/types.js
@@ -0,0 +1,64 @@
+exports.i32 = {
+	'category': 'i32',
+	'asText': function() {return 'i32';},
+	'equals': function(other) {return (other.category == 'i32');}
+};
+
+exports.func = function(returnType, paramTypes) {
+	return {
+		'category': 'function',
+		'returnType': returnType,
+		'paramTypes': paramTypes,
+		'paramsAsText': function() {
+			var paramAtoms = ['param'];
+			for (var i = 0; i < paramTypes.length; i++) {
+				paramAtoms.push(paramTypes[i].asText());
+			}
+			atoms.push('(' + paramAtoms.join(' ') + ')');
+			return '(' + paramAtoms.join(' ') + ')';
+		},
+		'returnTypeAsText': function() {
+			return '(result ' + returnType.asText() + ')';
+		},
+		'asText': function() {
+			var atoms = ['func'];
+			if (this.paramTypes.length) {
+				atoms.push(this.paramsAsText());
+			}
+			if (this.returnType.category != 'void') {
+				atoms.push(this.returnTypeAsText());
+			}
+			return '(' + atoms.join(' ') + ')';
+		},
+		'equals': function(other) {
+			if (other.category != 'function') return false;
+			if (!other.returnType.equals(this.returnType)) return false;
+			if (other.paramTypes.length != this.paramTypes.length) return false;
+			for (var i = 0; i < this.paramTypes.length; i++) {
+				if (!other.paramTypes[i].equals(this.paramTypes[i])) return false;
+			}
+			return true;
+		}
+	};
+};
+
+function fromCType(typ) {
+	switch (typ.category) {
+		case 'int':
+			return exports.i32;
+		case 'function':
+			var paramTypes = [];
+			for (var i = 0; i < typ.paramTypes.length; i++) {
+				paramTypes.push(fromCType(typ.paramTypes[i]));
+			}
+			return exports.func(
+				fromCType(typ.returnType),
+				paramTypes
+			);
+		default:
+			throw util.format("Don't know how to convert %s to a WebAssembly type",
+				util.inspect(typ)
+			);
+	}
+}
+exports.fromCType = fromCType;
--- wasm_exporter/wasm_module.js
+++ wasm_exporter/wasm_module.js
@@ -1,13 +1,112 @@
+var util = require('util');
+
+var wasmTypes = require('./types');
+
+function quoteString(str) {
+	return '"' + str.replace(/[\\"']/g, '\\$&') + '"';
+}
+
+class FunctionDefinition {
+	constructor(name, typ, isExported) {
+		this.name = name;
+		this.type = typ;
+		this.isExported = isExported;
+	}
+
+	asText() {
+		var atoms = [
+			'func',
+			util.format('(;%d;)', this.functionIndex),
+			util.format('(type %d)', this.typeIndex),
+		];
+		if (this.type.paramTypes.length) {
+			atoms.push(this.type.paramsAsText());
+		}
+		if (this.type.returnType.category != 'void') {
+			atoms.push(this.type.returnTypeAsText());
+		}
+		return "  (" + atoms.join(' ') + ")\n";
+	}
+
+	static fromAbstractFunctionDefinition(fd) {
+		var typ = wasmTypes.fromCType(fd.type);
+		return new FunctionDefinition(fd.name, typ, fd.isExported);
+	}
+}
+
 class WasmModule {
+	constructor() {
+		this.types = [];
+		this.functions = [];
+		this.exports = [];
+	}
+
+	defineFunction(functionDefinition) {
+		/* look in this module's types list for a type matching this function's type,
+		or create one if not found */
+		var typeIndex = null;
+		for (var i = 0; i < this.types.length; i++) {
+			if (this.types[i].equals(functionDefinition.type)) {
+				typeIndex = i;
+				break;
+			}
+		}
+		if (typeIndex === null) {
+			typeIndex = this.types.length;
+			this.types[typeIndex] = functionDefinition.type;
+		}
+		functionDefinition.typeIndex = typeIndex;
+
+		var functionIndex = this.functions.length;
+		functionDefinition.functionIndex = functionIndex;
+		this.functions[functionIndex] = functionDefinition;
+
+		if (functionDefinition.isExported) {
+			this.exports.push(
+				[functionDefinition.name, functionIndex]
+			);
+		}
+	}
+
 	asText() {
 		var output = "(module\n";
-		output += ")";
+		var i;
+
+		for (i = 0; i < this.types.length; i++) {
+			output += util.format("  (type (;%d;) %s)\n", i, this.types[i].asText());
+		}
+
+		for (i = 0; i < this.functions.length; i++) {
+			output += this.functions[i].asText();
+		}
+
+		for (i = 0; i < this.exports.length; i++) {
+			output += util.format("  (export %s (func %d))\n",
+				quoteString(this.exports[i][0]),
+				this.exports[i][1]
+			);
+		}
+
+		output += ")\n";
 
 		return output;
 	}
 
-	static fromAbstractModule(abstractModule) {
-		return new WasmModule();
+	static fromAbstractModule(module) {
+		var wasm = new WasmModule();
+		for (var i = 0; i < module.declarations.length; i++) {
+			var declaration = module.declarations[i];
+
+			switch (declaration.declarationType) {
+				case 'FunctionDefinition':
+					var fd = FunctionDefinition.fromAbstractFunctionDefinition(declaration);
+					wasm.defineFunction(fd);
+					break;
+				default:
+					throw util.format("Unsupported declaration type: %s", declaration.declarationType);
+			}
+		}
+		return wasm;
 	}
 }
 exports.WasmModule = WasmModule;
