9f5638d43169b69e25e2350add33f474cf377087
Matt Westcott <matt@west.co.tt>
2017-07-30 18:10:09 +0100

    avoid redundant tee/drop sequences on assignment expressions whose results aren't used

--- wasm_exporter/compiler.js
+++ wasm_exporter/compiler.js
@@ -5,6 +5,9 @@ var types = require('./types');
 var instructions = require('./instructions');
 
 function compileExpression(expr, context, out) {
+	/* compile the code for evaluating 'expr' into 'out', and return the number of values
+	pushed onto the stack; this will usually be 1, but may be 0 if the expression is a void
+	function call or its resultIsUsed flag is false. */
 	var localIndex;
 
 	if (expr.isCompileTimeConstant) {
@@ -12,7 +15,7 @@ function compileExpression(expr, context, out) {
 			types.fromCType(expr.type),
 			expr.compileTimeConstantValue
 		));
-		return;
+		return 1;
 	}
 
 	switch (expr.expressionType) {
@@ -23,15 +26,20 @@ function compileExpression(expr, context, out) {
 				throw util.format("Variable not found: %s", util.inspect(expr.left.variable));
 			}
 			compileExpression(expr.right, context, out);
-			out.push(instructions.TeeLocal(localIndex));
-			return;
+			if (expr.resultIsUsed) {
+				out.push(instructions.TeeLocal(localIndex));
+				return 1;
+			} else {
+				out.push(instructions.SetLocal(localIndex));
+				return 0;
+			}
 		case 'VariableExpression':
 			localIndex = context.getIndex(expr.variable.id);
 			if (localIndex === null) {
 				throw util.format("Variable not found: %s", util.inspect(expr.variable));
 			}
 			out.push(instructions.GetLocal(localIndex));
-			return;
+			return 1;
 		default:
 			throw util.format(
 				"Unrecognised expression type %s: %s",
@@ -43,11 +51,12 @@ function compileExpression(expr, context, out) {
 }
 
 function compile(body, context, out) {
+	var j;
 	for (var i = 0; i < body.length; i++) {
 		var statement = body[i];
 		switch(statement.statementType) {
 			case 'DeclarationStatement':
-				for (var j = 0; j < statement.variableDeclarations.length; j++) {
+				for (j = 0; j < statement.variableDeclarations.length; j++) {
 					var variableDeclaration = statement.variableDeclarations[j];
 					var variable = variableDeclaration.variable;
 					var index = context.declareVariable(variable.id, types.fromCType(variable.type));
@@ -58,8 +67,11 @@ function compile(body, context, out) {
 				}
 				break;
 			case 'ExpressionStatement':
-				compileExpression(statement.expression, context, out);
-				out.push(instructions.Drop);
+				var pushCount = compileExpression(statement.expression, context, out);
+				/* drop any results that were pushed */
+				for (j = 0; j < pushCount; j++) {
+					out.push(instructions.Drop);
+				}
 				break;
 			case 'ReturnStatement':
 				if (statement.expression !== null) {
